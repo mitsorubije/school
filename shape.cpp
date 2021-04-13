1634ac6417dd839b3e43f6c6f6abca62bea05d46
mitsorubi <79535192+mitsorubije@users.noreply.github.com>
2021-04-13 20:41:35 +0200

    Add files via upload

--- shape.cpp
+++ shape.cpp
@@ -1,22 +1,36 @@
 #include <iostream>
 #include "shape.h"
 
+//testování virtuální metody
+void TestShapes(){
+    cout << "Testovani virtualni metody: " << endl;
+    Shape* s1 = new Square(3);
+    double area = s1->GetObsah();
+    cout << "Square obsah: " << area << endl;
+
+    Shape* s2 = new Circle(3);
+    double obvod = s2->GetObvod();
+    cout << "Circle obvod: " << obvod << endl;
+}
+
 int main() {
-    Shape * tvar = new Shape(9);
-    tvar->Print();
-    cout << endl;
+    //testování běžným způsobem:
 
     Square * ctverec = new Square(3);
     ctverec->Print();
     cout << endl;
 
-    Circle * kruznice = new Circle(2);
+    Circle * kruznice = new Circle(3);
     kruznice->Print();
     cout << endl;
 
     Rectangle * obdelnik = new Rectangle(3, 4);
     obdelnik->Print();
     cout << endl;
+
+    //testování virtuální metody:
+
+    TestShapes();
 
     return 0;
 }
--- shape.h
+++ shape.h
@@ -2,6 +2,8 @@
 #include <math.h>
 using namespace std; 
 
+//hotovo TODO přidat tem shapům metody pro obvod a obsah, přidat getobvod a getobsah jako virtualni metodu pro shape, otestovat podle kodu z teamsů
+
 class Shape
 {
 
@@ -15,6 +17,10 @@ public:
 
     ~Shape(){  
     }
+
+    virtual double GetObvod() = 0;
+
+    virtual double GetObsah() = 0;
 
     int GetStrany(){
         return this->_pocetStran;
@@ -31,22 +37,29 @@ class Square : public Shape
 {
 
 private:
-    int _obvod;
     int _delkaStrany;
 
 public:
     Square(int delkaStrany) : Shape(4){
         this->_delkaStrany = delkaStrany;
-        this->_obvod = 4 * this->_delkaStrany;
     }
 
     ~Square(){
     }
 
+    double GetObvod(){
+        return 4 * this->_delkaStrany;
+    }
+
+    double GetObsah(){
+        return _delkaStrany * _delkaStrany;
+    }
+
     void Print(){
         cout << "Pocet stran: " << this->GetStrany() << endl;
         cout << "Delka strany: " << this->_delkaStrany << endl;
-        cout << "Obvod: " << this->_obvod << endl;
+        cout << "Obvod: " << this->GetObvod() << endl;
+        cout << "Obsah: " << this->GetObsah() << endl;
     }
 
 
@@ -57,21 +70,28 @@ class Circle : public Shape
 {
 
 private:
-    float _obvod;
     int _polomer;
 
 public:
     Circle(int polomer) : Shape(0){
         this->_polomer = polomer;
-        this->_obvod = 2 * M_PI * this->_polomer;
     }
 
     ~Circle(){
     }
 
+    double GetObvod(){
+        return 2 * M_PI * this->_polomer;
+    }
+
+    double GetObsah(){
+        return M_PI * pow(_polomer, 2);
+    }
+
     void Print(){
         cout << "Polomer: " << this->_polomer << endl;
-        cout << "Obvod: " << this->_obvod << endl;
+        cout << "Obvod: " << this->GetObvod() << endl;
+        cout << "Obsah: " << this->GetObsah() << endl;
     }
 
 
@@ -82,7 +102,6 @@ class Rectangle : public Shape
 {
 
 private:
-    int _obvod;
     int _delkaStrany1;
     int _delkaStrany2;
 
@@ -90,16 +109,24 @@ public:
     Rectangle(int delkaStrany1, int delkaStrany2) : Shape(4){
         this->_delkaStrany1 = delkaStrany1;
         this->_delkaStrany2 = delkaStrany2;
-        this->_obvod = 2 * (this->_delkaStrany1 + this->_delkaStrany2);
     }
 
     ~Rectangle(){
     }
 
+    double GetObvod(){
+        return 2 * (this->_delkaStrany1 + this->_delkaStrany2);
+    }
+
+    double GetObsah(){
+        return _delkaStrany1 * _delkaStrany2;
+    }
+
     void Print(){
         cout << "Pocet stran: " << this->GetStrany() << endl;
         cout << "Delka stran: " << this->_delkaStrany1 << "; " << this->_delkaStrany2 << endl;
-        cout << "Obvod: " << this->_obvod << endl;
+        cout << "Obvod: " << this->GetObvod() << endl;
+        cout << "Obsah: " << this->GetObsah() << endl;
     }
 
 
