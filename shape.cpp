26831a8cfa42e620bb07bd0690d537325f1ad38f
mitsorubi <79535192+mitsorubije@users.noreply.github.com>
2021-05-01 16:13:11 +0200

    Add files via upload
    logger

--- shape.cpp
+++ shape.cpp
@@ -13,6 +13,113 @@ void TestShapes(){
     cout << "Circle obvod: " << obvod << endl;
 }
 
+void TestShapeSort(){
+    Shape* s1 = new Square(3);
+    Shape* s2 = new Circle(3);
+    Shape* s3 = new Rectangle(3, 4);
+    Shape* s4 = new Square(6);
+    Shape* s5 = new Circle(9);
+    Shape* s6 = new Rectangle(1, 4);
+
+    Shape* pole[6] = {s1,s2,s3,s4,s5,s6};
+
+    cout << "Poradi pred serazenim: " << endl;
+    for(int x = 0; x < 6; x++){
+        cout << pole[x]->GetObvod() << endl;
+    }
+    cout << endl;
+
+
+    for(int i = 0; i < 5; i++){    
+        for(int x = 0; x < 5-i; x++){ 
+            if(pole[x]->GetObvod() > pole[x+1]->GetObvod()){
+                Shape* temp = pole[x];
+                pole[x] = pole[x+1];
+                pole[x+1] = temp;
+            }
+        }
+    }
+
+    cout << "Poradi po serazeni: " << endl;
+    for(int x = 0; x < 6; x++){
+        cout << pole[x]->GetObvod() << endl;
+    }
+    cout << endl;
+
+}
+
+void Test3DShapes(){
+    cout << "Testovani virtualni metody u 3D shapes: " << endl;
+    ThirdDimensionObject* s1 = new Cube(3);
+    double volume1 = s1->GetVolume();
+    cout << "Cube objem: " << volume1 << endl;
+
+    ThirdDimensionObject* s2 = new Sphere(3);
+    double volume2 = s2->GetVolume();
+    cout << "Ball objem: " << volume2 << endl;
+
+    ThirdDimensionObject* s3 = new Block(3,3,4);
+    double volume3 = s3->GetVolume();
+    cout << "Block objem: " << volume3 << endl;
+}
+
+void Test3DShapeSort(){
+    ThirdDimensionObject* s1 = new Cube(3);
+    ThirdDimensionObject* s2 = new Sphere(3);
+    ThirdDimensionObject* s3 = new Block(3, 4, 3);
+    ThirdDimensionObject* s4 = new Cube(6);
+    ThirdDimensionObject* s5 = new Sphere(9);
+    ThirdDimensionObject* s6 = new Block(1, 4, 2);
+
+    ThirdDimensionObject* pole[6] = {s1,s2,s3,s4,s5,s6};
+
+    cout << "Poradi pred serazenim: " << endl;
+    for(int x = 0; x < 6; x++){
+        cout << pole[x]->GetVolume() << endl;
+    }
+    cout << endl;
+
+
+    for(int i = 0; i < 5; i++){    
+        for(int x = 0; x < 5-i; x++){ 
+            if(pole[x]->GetVolume() > pole[x+1]->GetVolume()){
+                ThirdDimensionObject* temp = pole[x];
+                pole[x] = pole[x+1];
+                pole[x+1] = temp;
+            }
+        }
+    }
+
+    cout << "Poradi po serazeni: " << endl;
+    for(int x = 0; x < 6; x++){
+        cout << pole[x]->GetVolume() << endl;
+    }
+    cout << endl;
+
+}
+
+void TestLogger(){
+    HistoryLogger::ClearLogs();
+
+    HistoryLogger::PrintLog(); //nic
+    cout << endl;
+
+    Shape* s1 = new Square(3);
+    Shape* s2 = new Circle(3);
+    Shape* s3 = new Rectangle(3, 4);
+    Shape* s4 = new Square(6);
+
+    //HistoryLogger::PrintLog(); //tady to vypíše ty 4 shapes
+    //cout << endl;
+
+    Shape* s5 = new Circle(5);
+    Shape* s6 = new Circle(4);
+    Shape* s7 = new Circle(1);
+
+    HistoryLogger::PrintLog(); //tady to vypíše s3 až a7 (posledních 5)
+
+}
+
 int main() {
     //testování běžným způsobem:
 
@@ -31,6 +138,25 @@ int main() {
     //testování virtuální metody:
 
     TestShapes();
+    cout << endl;
+
+    //serazeni:
+
+    //TestShapeSort();
+
+    Cube* cube1 = new Cube(4);
+    cout << cube1->GetVolume() << endl;
+
+    Sphere* sphere1 = new Sphere(3);
+    cout << sphere1->GetVolume() << endl;
+
+    Test3DShapes();
+
+    //sort 3d itemu:
+    Test3DShapeSort();
+
+    //logger test:
+    TestLogger();
 
     return 0;
 }
--- shape.h
+++ shape.h
@@ -4,7 +4,75 @@ using namespace std;
 
 //hotovo TODO přidat tem shapům metody pro obvod a obsah, přidat getobvod a getobsah jako virtualni metodu pro shape, otestovat podle kodu z teamsů
 
-class Shape
+//hotovo TODO: vyuzit polymorfismus - pole 5-10 objektů a seřadit je
+
+//hotovo TODO: logger interface
+
+class HistoryItem
+{
+    public:
+        virtual void Print() = 0;
+};
+
+class HistoryLogger
+{
+    private:
+        static HistoryItem* _items[5];
+        static int _counter;
+    public:    
+        static void log(HistoryItem* item){
+            if(HistoryLogger::_counter < 5){
+                HistoryLogger::_items[HistoryLogger::_counter] = item;
+                HistoryLogger::_counter++;
+            }
+            else{
+                HistoryLogger::remove(HistoryLogger::_items[0]);
+                HistoryLogger::log(item);
+            }
+        }
+
+        static void remove(HistoryItem* item){
+            for(int x = 0; x < 5; x++){
+                if(HistoryLogger::_items[x] == item){
+                    HistoryLogger::_items[x] = NULL;
+                    int subx = x;
+                    for(int y = 1; y < HistoryLogger::_counter - x; y++){
+                        HistoryLogger::_items[subx] = HistoryLogger::_items[subx + 1];
+                        subx++;
+                    }
+                    HistoryLogger::_counter--;
+                    x = 10;
+                }
+            }
+        }
+
+        static void removeLast(){
+            int pos = HistoryLogger::_counter - 1;
+            HistoryLogger::_items[pos] = NULL;
+            HistoryLogger::_counter--;
+        }
+
+        static void PrintLog(){
+            for(int x = 0; x < HistoryLogger::_counter; x++){
+                HistoryLogger::_items[x]->Print();
+                cout << endl;
+            }
+            if(HistoryLogger::_counter == 0){
+                cout << "HistoryLogger nema zadny zaznam" << endl;
+            }
+        }
+
+        static void ClearLogs(){
+            for(int x = 0; x < HistoryLogger::_counter; x++){
+                HistoryLogger::_items[x] = NULL;
+            }
+            HistoryLogger::_counter = 0;
+        }
+};
+HistoryItem* HistoryLogger::_items[5] = {0};
+int HistoryLogger::_counter = 0;
+
+class Shape : public HistoryItem
 {
 
 private:
@@ -13,9 +81,11 @@ private:
 public:
     Shape(int pocetStran){
         this->_pocetStran = pocetStran;
+        HistoryLogger::log(this);
     }
 
     ~Shape(){  
+        HistoryLogger::remove(this);
     }
 
     virtual double GetObvod() = 0;
@@ -26,7 +96,7 @@ public:
         return this->_pocetStran;
     }
 
-    void Print(){
+    virtual void Print(){
         cout << "Pocet stran: " << this->GetStrany() << endl;
     }
 
@@ -38,6 +108,11 @@ class Square : public Shape
 
 private:
     int _delkaStrany;
+
+protected:
+    int GetA(){
+        return _delkaStrany;
+    }    
 
 public:
     Square(int delkaStrany) : Shape(4){
@@ -72,6 +147,11 @@ class Circle : public Shape
 private:
     int _polomer;
 
+protected:
+    int GetPolomer(){
+        return _polomer;
+    }
+
 public:
     Circle(int polomer) : Shape(0){
         this->_polomer = polomer;
@@ -105,6 +185,14 @@ private:
     int _delkaStrany1;
     int _delkaStrany2;
 
+protected:
+    int GetA(){
+        return _delkaStrany1;
+    }
+    int GetB(){
+        return _delkaStrany2;
+    }
+
 public:
     Rectangle(int delkaStrany1, int delkaStrany2) : Shape(4){
         this->_delkaStrany1 = delkaStrany1;
@@ -127,6 +215,69 @@ public:
         cout << "Delka stran: " << this->_delkaStrany1 << "; " << this->_delkaStrany2 << endl;
         cout << "Obvod: " << this->GetObvod() << endl;
         cout << "Obsah: " << this->GetObsah() << endl;
+    }
+
+
+};
+
+class ThirdDimensionObject
+{
+
+public:
+
+    virtual double GetVolume() = 0;
+
+};
+
+class Cube : public Square, public ThirdDimensionObject
+{
+
+public:
+    Cube(int delkaStrany) : Square(delkaStrany){
+    }
+
+    ~Cube(){
+    }
+
+    double GetVolume(){
+        return GetA() * GetA() * GetA();
+    }
+
+
+};
+
+class Sphere : public Circle, public ThirdDimensionObject
+{
+
+public:
+    Sphere(int delkaStrany) : Circle(delkaStrany){
+    }
+
+    ~Sphere(){
+    }
+
+    double GetVolume(){
+        return (4 / 3) * M_PI * pow(GetPolomer(), 3);
+    }
+
+
+};
+
+class Block : public Rectangle, public ThirdDimensionObject
+{
+private:
+    int _delkaStrany3;
+
+public:
+    Block(int delkaStrany1, int delkaStrany2, int delkaStrany3) : Rectangle(delkaStrany1, delkaStrany2){
+        this->_delkaStrany3 = delkaStrany3;
+    }
+
+    ~Block(){
+    }
+
+    double GetVolume(){
+        return GetA() * GetB() * this->_delkaStrany3;
     }
 
 
